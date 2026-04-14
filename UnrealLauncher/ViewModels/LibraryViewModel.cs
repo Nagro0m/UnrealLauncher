97a26eb3c9307ed57d4f1e6910efadfc80182110
noahostettler <98475843+noahostettler@users.noreply.github.com>
2026-04-14 14:58:52 +0200

    poush

--- UnrealLauncher/ViewModels/LibraryViewModel.cs
+++ UnrealLauncher/ViewModels/LibraryViewModel.cs
@@ -6,6 +6,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
+using System.Windows.Controls;
 using System.Windows.Input;
 using UnrealLauncher.Models;
 using UnrealLauncher.Utils;
@@ -14,9 +15,11 @@ namespace UnrealLauncher.ViewModels
 {
     public class LibraryViewModel : ViewModelBase
     {
-        public RelayCommand LaunchCommand => new RelayCommand(_execute => LaunchProject(_execute as ProjectModel));
+        public RelayCommand LaunchProjectCommand => new RelayCommand(_execute => LaunchProject(_execute as ProjectModel));
+        public RelayCommand LaunchEngineCommand => new RelayCommand(_execute => LaunchEngine(_execute as EngineVersionModel));
         public ObservableCollection<ProjectModel> Projects { get; set; }
-
+        public ObservableCollection<EngineVersionModel> EngineVersions { get; set; }
+        public ObservableCollection<FabModel> FabItems { get; set; }
 
         public LibraryViewModel()
         {
@@ -30,8 +33,27 @@ namespace UnrealLauncher.ViewModels
                 @"D:\2025-2026\Unreal\Cours"
             };
 
-            List<ProjectModel> foundProjects = UnrealProjectsFinder.FindProjects(roots);
+            List<ProjectModel> foundProjects = UnrealManager.FindProjects(roots);
             Projects = new ObservableCollection<ProjectModel>(foundProjects);
+
+            List<EngineVersionModel> _foundEngines = UnrealManager.FindInstalledEngines();
+            EngineVersions = new ObservableCollection<EngineVersionModel>(_foundEngines);
+
+            FabItems = new ObservableCollection<FabModel>
+            {
+                new FabModel
+                {
+                    Name = "Abandoned Factory",
+                    ImagePath = "Images/factory.png",
+                    ActionText = "Add To Project"
+                },
+                new FabModel
+                {
+                    Name = "City Sample",
+                    ImagePath = "Images/city.png",
+                    ActionText = "Create Project"
+                }
+            };
         }
 
         void LaunchProject(ProjectModel project)
@@ -42,7 +64,19 @@ namespace UnrealLauncher.ViewModels
                 return;
             }
 
-            UnrealProjectsFinder.LaunchProject(project);
+            UnrealManager.LaunchProject(project);
+        }
+
+        void LaunchEngine(EngineVersionModel version)
+        {
+            if (version == null)
+            {
+                MessageBox.Show("Engine Version not found");
+                return;
+            }
+
+                MessageBox.Show("Engine Version  found");
+            UnrealManager.LaunchEngine(version);
         }
 
     }
